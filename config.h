17171137be92cc77966decbd9840031bfd9ce1b9
sb542 <simonspruzz@gmail.com>
2014-02-09 18:29:39 +0000

    Create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define LISTENADDRESS   "127.0.0.1"
+#define DEFAULTPORT     1234
+#define RSA_KEYFILE     "pubkey"
+#define LOGFILE         "sshpot_auth.log"
+#define DEBUG           1
+
+#endif
